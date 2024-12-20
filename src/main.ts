1711ffbe681e1ca2210b45014233a009c28c3ad3
Alexsandro Dos Santos junior <alexsandro_azuras@hotmail.com>
2024-12-20 18:21:11 -0300

    Atualização web v3

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-
-// import { AppModule } from './app.module';
-// import { ZodFilter } from './shared/filters/zod.filter';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors();
-//   app.useGlobalFilters(new ZodFilter());
-
-//   await app.listen(process.env.PORT || 3000);
-// }
-
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
 import { AppModule } from './app.module';
